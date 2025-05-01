61f19c5289a05cfae25048cf2334548d571f5cb4
halitykc <87850150+halitykc@users.noreply.github.com>
2025-05-01 23:46:11 +0300

    Create last_word.c

---
+++ examtrain/last_word.c
@@ -0,0 +1,18 @@
+#include <unists.h>
+int main() {
+    // Write C code here
+    char s1[] = "this        ...       is sparta, then again, maybe    not";
+ 
+    int i = 0;
+    while (s1[i])
+        i++;
+    i--;
+    while (s1[i] == ' ' || s1[i] == '\t')
+        i--;
+    while (s1[i] != ' ' && s1[i] != '\t')
+        i--;
+    i++;
+    while(s1[i] && s1[i] != ' ' && s1[i] !='\t')
+        write(1,&s1[i++],1);
+
+}
